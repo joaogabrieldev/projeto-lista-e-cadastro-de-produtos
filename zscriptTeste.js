2f49b0cb1935ce7908d5008a403fd0be35408b61
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2025-05-21 15:13:51 -0300

    Initial commit

---
+++ zscriptTeste.js
@@ -0,0 +1,25 @@
+var produtos = [
+  { nome: "Mouse", valor: 50 },
+  { nome: "Teclado", valor: 150 },
+  { nome: "Monitor", valor: 1000 },
+];
+
+produtos.sort(function (a, b) {
+  return b.valor - a.valor;
+});
+
+console.log(produtos);
+
+/*
+
+
+
+      
+
+
+
+
+
+
+
+*/
